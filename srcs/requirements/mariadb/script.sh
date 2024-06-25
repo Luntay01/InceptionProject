197439b9097ced41bb46b1fb09a2ddff9bc1ceef
kyle <kylemordaunt@gmail.com>
2024-06-26 02:11:16 +0930

    hopefuly fixed and working all container seem to be running

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-service mysql start
-mysql < /etc/mysql/init.sql
-mysqld
+# Initialize the MariaDB data directory
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+fi
+
+# Start the MariaDB server
+mysqld_safe &
+
+# Wait for the MariaDB server to start
+until mysqladmin ping --silent; do
+    echo 'waiting for mysql to be connectable...'
+    sleep 2
+done
+
+# Run the initialization script to create the database and user
+if [ ! -f /var/lib/mysql/initialized ]; then
+    mysql < /etc/mysql/init.sql
+    touch /var/lib/mysql/initialized
+fi
+
+# Keep the container running
+tail -f /dev/null
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
-./wp-cli.phar core install --url=$DOMAIN_NAME --title=inception --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+
+# Wait for MariaDB to be ready
+until mysqladmin ping -h"$MYSQL_HOST" --silent; do
+    echo 'waiting for mysql to be connectable...'
+    sleep 2
+done
+
+# Download and set up WordPress
+if [ ! -f /var/www/html/wp-config.php ]; then
+    cd /var/www/html
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    chmod +x wp-cli.phar
+    ./wp-cli.phar core download --allow-root
+    ./wp-cli.phar config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
+    ./wp-cli.phar core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+fi
+
+# Start PHP-FPM
 php-fpm8.2 -F
